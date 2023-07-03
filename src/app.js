2db764f0b23d5bce8c5d15f625f2653c7c4ca308
Maria Angelica Salas Jimenez <agiesalasjimenez@gmail.com>
2023-07-03 00:38:09 -0500

    Showing the current temperature

---
+++ src/app.js
@@ -0,0 +1,18 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "4031af3089baee44t6720aff433o521e";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=New York&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
